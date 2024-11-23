d5fcd8228106f35d4b9d115c7ab04c450952ed7a
Gonzalo De Spírito <gonza@fintual.com>
2024-11-23 17:07:58 -0300

    feat: categorize movements

--- app/Http/Controllers/GetFintocMovementsController.php
+++ app/Http/Controllers/GetFintocMovementsController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\FintocBankLink;
 use App\Models\Movement;
+use App\Models\User;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
@@ -32,5 +33,7 @@ class GetFintocMovementsController extends Controller
                 ]);
             }
         }
+
+        dispatch(new \App\Jobs\CategorizeMovements($request->user()));
     }
 }
