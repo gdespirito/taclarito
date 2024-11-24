746a4f22dc716ee306b555c1ce0bf37abb79972b
Vokturz <victornavarrokp47@gmail.com>
2024-11-24 02:22:49 -0300

    fix: remove id from expense item wrapped

--- llm/app/api/v1/models.py
+++ llm/app/api/v1/models.py
@@ -58,7 +58,6 @@ class ExpenseItemWrapped(BaseModel):
     """
     Represents an individual expense/transfer entry
     """
-    id: str = Field(description="Unique identifier for the expense")
     amount: float = Field(description="Amount spent in Chilean pesos",gt=0)
     description: str = Field(descriptiodn="Description of the Item")
     category: ExpenseCategoryWrapped = Field(description="Category of the expense")
